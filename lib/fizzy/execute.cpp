07976699b04a2bdcaea49be7d47cb74410e4e086
Alex Beregszaszi <alex@rtfs.hu>
2020-10-08 19:02:03 +0100

    Use the fcopysign helper in fceil and fnearest

--- lib/fizzy/execute.cpp
+++ lib/fizzy/execute.cpp
@@ -327,6 +327,25 @@ inline double fneg(double value) noexcept
 }
 
 template <typename T>
+T fcopysign(T a, T b) noexcept = delete;
+
+template <>
+inline float fcopysign(float a, float b) noexcept
+{
+    const auto a_u = bit_cast<uint32_t>(a);
+    const auto b_u = bit_cast<uint32_t>(b);
+    return bit_cast<float>((a_u & F32AbsMask) | (b_u & F32SignMask));
+}
+
+template <>
+inline double fcopysign(double a, double b) noexcept
+{
+    const auto a_u = bit_cast<uint64_t>(a);
+    const auto b_u = bit_cast<uint64_t>(b);
+    return bit_cast<double>((a_u & F64AbsMask) | (b_u & F64SignMask));
+}
+
+template <typename T>
 inline T fceil(T value) noexcept
 {
     static_assert(std::is_floating_point_v<T>);
@@ -353,7 +372,7 @@ inline T ffloor(T value) noexcept
     // the __builtin_floor() outputs -0 where it should +0.
     // The following workarounds the issue by using the fact that the sign of
     // the output must always match the sign of the input value.
-    return std::copysign(result, value);
+    return fcopysign(result, value);
 }
 
 template <typename T>
@@ -384,7 +403,7 @@ T fnearest(T value) noexcept
     // This implementation is based on adjusting the result produced by trunc() by +-1 when needed.
     const auto t = std::trunc(value);
     if (const auto diff = std::abs(value - t); diff > T{0.5} || (diff == T{0.5} && !is_even(t)))
-        return t + std::copysign(T{1}, value);
+        return t + fcopysign(T{1}, value);
     else
         return t;
 }
@@ -425,24 +444,6 @@ inline T fmax(T a, T b) noexcept
     return a < b ? b : a;
 }
 
-template <typename T>
-T fcopysign(T a, T b) noexcept = delete;
-
-template <>
-inline float fcopysign(float a, float b) noexcept
-{
-    const auto a_u = bit_cast<uint32_t>(a);
-    const auto b_u = bit_cast<uint32_t>(b);
-    return bit_cast<float>((a_u & F32AbsMask) | (b_u & F32SignMask));
-}
-
-template <>
-inline double fcopysign(double a, double b) noexcept
-{
-    const auto a_u = bit_cast<uint64_t>(a);
-    const auto b_u = bit_cast<uint64_t>(b);
-    return bit_cast<double>((a_u & F64AbsMask) | (b_u & F64SignMask));
-}
 
 __attribute__((no_sanitize("float-cast-overflow"))) inline constexpr float demote(
     double value) noexcept
