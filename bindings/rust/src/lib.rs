631820a94d4f5ba937611366c1bfce6a67fb17c4
Alex Beregszaszi <alex@rtfs.hu>
2021-03-11 15:18:37 +0000

    f

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -183,6 +183,11 @@ fn create_function_import_list(host_functions: &[HostFunction]) -> Vec<sys::Fizz
         function: Some(host_callback),
         context: std::ptr::null_mut(),
     };
+    print!(
+        "{:?} {:?}",
+        host_function.module.as_bytes_with_nul(),
+        host_function.name.as_bytes_with_nul()
+    );
     vec![sys::FizzyImportedFunction {
         module: host_function.module.as_ptr(),
         name: host_function.name.as_ptr(),
@@ -197,14 +202,14 @@ impl Module {
         debug_assert!(!self.0.is_null());
         let mut err = FizzyErrorBox::new();
         let host_fn1 = HostFunction {
-            module: CString::new("env").unwrap(),
-            name: CString::new("print").unwrap(),
+            module: CString::new("env").expect("cstring to work"),
+            name: CString::new("print").expect("cstring to work"),
             inputs: vec![],
             output: sys::FizzyValueTypeVoid,
             index: 0,
         };
         let import_list = create_function_import_list(&[host_fn1]);
-        println!("{}", import_list.len());
+        println!("{:?} {}", import_list.as_ptr(), import_list.len());
         let ptr = unsafe {
             sys::fizzy_resolve_instantiate(
                 self.0,
